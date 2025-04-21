52eb46b5aa0389c345ee74de4c515a7a313f0610
Vikkasss <valedentsova@edu.hse.ru>
2025-04-21 12:02:51 +0300

    add mini-hw

--- Moscow_zoo_part2/Moscow_zoo_part2/Program.cs
+++ Moscow_zoo_part2/Moscow_zoo_part2/Program.cs
@@ -1,13 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Moscow_zoo_part2.Infrastructure.Repositories;
+using Moscow_zoo_part2.Domain.Interfaces;
 
-namespace Moscow_zoo_part2
+var builder = WebApplication.CreateBuilder(args);
+
+// Регистрация репозиториев (In-Memory)
+builder.Services.AddSingleton<IAnimalRepository, InMemoryAnimalRepository>();
+builder.Services.AddSingleton<IEnclosureRepository, InMemoryEnclosureRepository>();
+builder.Services.AddSingleton<IFeedingScheduleRepository, InMemoryFeedingScheduleRepository>();
+builder.Services.AddSingleton<IEventRepository, InMemoryEventRepository>();
+
+// Регистрация MediatR
+builder.Services.AddMediatR(cfg => 
+    cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
+
+// Добавление контроллеров
+builder.Services.AddControllers();
+
+// Настройка Swagger
+builder.Services.AddSwaggerGen(c =>
 {
-    internal class Program
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Zoo API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Включение Swagger UI в development-режиме
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => 
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Zoo API v1");
+    });
 }
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
